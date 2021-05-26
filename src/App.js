9c6c3363f167bffc06cca762ecc3d67117eadc48
ObedGarcia64 <a1119150014@utch.edu.mx>
2021-05-26 01:50:56 -0600

    Hero was added with the New Badge

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Layout from "./pages/layout/Layout"
 import Main from "./pages/main/main"
-import Badge from "./components/Badge"
+import NewBadge from "./pages/NewBadge/NewBadge"
 
 function App() {
   return (
     <React.Fragment>
       <Layout>
-        <Badge 
-        profile_picture="https://upload.wikimedia.org/wikipedia/commons/9/9a/200126_Dua_Lipa_on_the_2020_Grammys_Red_Carpet.jpg">
-          
-        </Badge>
+        <NewBadge></NewBadge>
       </Layout>
     </React.Fragment>
   );
