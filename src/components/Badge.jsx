f8b888c2b768b103e5d7542450f930b605250a92
ObedGarcia64 <a1119150014@utch.edu.mx>
2021-05-26 02:41:27 -0600

    The page is now interactive

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -16,13 +16,13 @@ class Badge extends Component {
                         <div className="Badge__userImage">
                             <img src={this.props.profile_picture} alt="Profile_picture"  />
                         </div>
-                        <h4 className="text-center mt-5">{this.props.age}<i> {this.props.age}</i></h4>
+                        <h4 className="text-center mt-5">{this.props.name}<i> {this.props.age}</i></h4>
                         <p className="text-center">{this.props.city}</p>
                     </div>
                     <div className="Badge__info container pt-3">
                         <div className="row">
                             <div className="col">
-                                <h4 className="text-center fw bold">{this.props.fpllowers}</h4>
+                                <h4 className="text-center fw bold">{this.props.followers}</h4>
                                 <p className="text-center">Followers</p>
                             </div>
                             <div className="col">
