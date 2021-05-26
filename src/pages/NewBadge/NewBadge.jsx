9ef8bf3976510074dba18735d68f374a8b433668
ObedGarcia64 <a1119150014@utch.edu.mx>
2021-05-26 02:21:32 -0600

    NewBadge is added

--- src/pages/NewBadge/NewBadge.jsx
+++ src/pages/NewBadge/NewBadge.jsx
@@ -1,13 +1,41 @@
 import React from "react"
 import Hero from "../../components/Hero"
+import Badge from "../../components/Badge"
 import "./NewBadge.css"
+import { name } from "faker"
 
 class NewBadge extends React.Component{
+
+    state = {
+        loading : false,
+        error: null,
+        form:{
+            header_picture:"",
+            profile_picture:"",
+            name:"",
+            Age:"",
+            city:"",
+            followers:"",
+            likes:"",
+            post:"",
+        }
+    }
+
     render(){
         return(
             <React.Fragment>
                 <Hero h={"15vh"}></Hero>
-                <h1>New Badge</h1>
+                <div className="container">
+                    <div className="col">
+                        <Badge
+                            header_picture = {this.state.form.header_picture || "https://coatepec.gob.mx/wp-content/uploads/2019/09/4K-Moving-Stars-Live-Wallpaper-1.jpg"}
+                            profile_picture = {this.state.form.profile_picture || "https://lumiere-a.akamaihd.net/v1/images/be_hte_title_cover_final_ed_web_f1ba4992.jpeg"}
+                        ></Badge>
+                    </div>
+                    <div className="col">
+
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
