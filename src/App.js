013aa257ddac2cd52d89d612fbc7e1f749f36922
ObedGarcia64 <a1119150014@utch.edu.mx>
2021-06-08 17:47:55 -0600

    Badges created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from "./pages/layout/Layout"
 import FormLogin from './pages/lgin/lgin';
 import Main from "./pages/main/main"
 import NewBadge from "./pages/NewBadge/NewBadge"
+import Badges from "./pages/Badges/Badges"
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={Main}></Route>
           <Route exact path="/new" component={NewBadge}></Route>
+          <Route exact path="/badges" component={Badges}></Route>
           <Route exact path="/newone" component={FormLogin}></Route>
-          <Route exact path="/error" component={err}></Route>
+          <Route component={err}></Route>
         </Switch>
       </Layout>
     </BrowserRouter>
