c29b5e083c1bbe22c3a802fb564d9686730f7039
ObedGarcia64 <a1119150014@utch.edu.mx>
2021-06-08 19:29:17 -0600

    SkeletonItem was added

--- src/pages/Badges/Badges.jsx
+++ src/pages/Badges/Badges.jsx
@@ -1,9 +1,46 @@
-import React from "react"
-import react from "react"
-import "./Badges.css"
+import React from "react";
+import SkeletonItem from "../../components/SkeletonItem"
+import api from "../../libs/api"
+import "./Badges.css";
 
 class Badges extends React.Component{
+
+    
+    state={
+        loading:true,
+        error: null,
+        data: undefined
+    }
+
+    componentDidMount(){
+        this.fetchData();
+        this.setFetchInterval();
+    }
+
+    fetchData = async() =>{
+        this.setState({loading: true, error: null})
+        try{
+            const data = await api.badges.list();
+            data.reverse()
+            this.setState({loading: false, data: data})
+
+        }catch(error){
+            this.setState({loading: false, error: error, data: []})
+        }
+    }
+
+    setFetchInterval(){
+        this.interval = setInterval(this.fetchData, 3000)
+    }
+
+    componentWillMount(){
+        clearInterval(this.interval)
+    }
+
     render(){
+        if(this.state.loading===true && !this.state.data){
+            return <SkeletonItem></SkeletonItem>
+        }
         return(
             <React.Fragment>
                 <h1>All Badges</h1>
@@ -11,5 +48,5 @@ class Badges extends React.Component{
         );
     }
 }
-
+ 
 export default Badges;
